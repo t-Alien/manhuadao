ad81c1e1c320ce335a57d37b02633af7aff8899c
t-Alien <1091602341@qq.com>
2019-08-19 22:33:13 +0800

    Ranking未完成

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import {} from 'vant';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import { Tab, Tabs } from 'vant'
 
-import "./assets/styles/base.scss";
+import './assets/styles/base.scss'
+import 'vant/lib/index.css'
 
-Vue.config.productionTip = false;
+Vue.use(Tab).use(Tabs)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
