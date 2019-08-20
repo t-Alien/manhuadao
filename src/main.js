15bfc91d48dbb980249a607a595f4934263632af
mlpzaq <2278219987@qq.com>
2019-08-20 17:19:21 +0800

    注册页面完成

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import VueResource from 'vue-resource'
-
+import { Tab, Tabs, Toast } from 'vant'
 import './assets/styles/base.scss'
 
-Vue.use(VueResource)
+Vue.use(Tab)
+  .use(Tabs)
+  .use(Toast)
 
 // Vue.config.productionTip = false
 
