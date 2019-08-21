2bf069148a4e1fd4684c73e9bebe4e2a36ff7845
Cyd-Dowson <416355003@qq.com>
2019-08-21 21:58:24 +0800

    搜索解决冲突

--- src/main.js
+++ src/main.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-<<<<<<< HEAD
 import { Swipe, SwipeItem ,Grid, GridItem,Image, Tab, Tabs, Toast,Search } from 'vant';
-=======
-import { Swipe, SwipeItem, Grid, GridItem, Image, Tab, Tabs, Toast } from 'vant'
->>>>>>> 7e80d03c4ec80569217e608335eba4c15fc753e9
 
 import TabBar from './components/TabBar/index'
 
@@ -15,16 +11,6 @@ import 'vant/lib/index.css'
 Vue.config.productionTip = false
 
 Vue.use(Swipe)
-<<<<<<< HEAD
-.use(SwipeItem)
-.use(Grid)
-.use(GridItem)
-.use(Image)
-.use(Tab)
-.use(Tabs)
-.use(Toast)
-.use(Search)
-=======
   .use(SwipeItem)
   .use(Grid)
   .use(GridItem)
@@ -33,7 +19,7 @@ Vue.use(Swipe)
   .use(Tabs)
   .use(Toast)
   .use(TabBar)
->>>>>>> 7e80d03c4ec80569217e608335eba4c15fc753e9
+  .use(Search)
 
 new Vue({
   router,
